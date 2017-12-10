75e39bc4271628d9fc3e8ca8256b3794863b459d
phagocyte <githubphagocyte@gmail.com>
2017-12-10 21:17:24 +0000

    Make initial randomised metal distribution show.

--- script.js
+++ script.js
@@ -1,44 +1,142 @@
 class Simulation {
-	constructor(
-		radiusInput,
-		numberOfMetalPiecesInput,
-		shapeCanvas,
-		temperatureCanvas
-	) {
-		this.radiusInput = radiusInput
-		this.numberOfMetalPiecesInput = numberOfMetalPiecesInput
-		this.shapeCanvas = shapeCanvas
-		this.temperatureCanvas = temperatureCanvas
-		this.restart()
+	constructor(heatsink, timerTracker) {
+		this.heatsink = heatsink
+		this.timerTracker = timerTracker
+		
+		this.optimiseScore()		
 	}
 	
-	restart() {
-		this.radius = this.radiusInput.value
-		this.numberOfMetalPieces = this.numberOfMetalPiecesInput.value
-		this.width = this.radius * 2 + 1
-		this.shapeCanvas.width = this.width
-		this.shapeCanvas.height = this.width
-		this.temperatureCanvas.width = this.width
-		this.temperatureCanvas.height = this.width
+	optimiseScore() {
 	}
 }
 
+class Heatsink {
+	constructor(temperatureMap, heatsinkCanvas, radius, numberOfMetalPieces) {
+		this.radius = radius
+		this.numberOfMetalPieces = numberOfMetalPieces
+		this.temperatureMap = temperatureMap
+		this.canvas = heatsinkCanvas
+		
+		this.width = this.radius * 2 + 1
+		this.canvas.width = this.width
+		this.canvas.height = this.width
+		
+		this.context = this.canvas.getContext('2d')
+		this.imageData = this.context.createImageData(this.canvas.width, this.canvas.height)
+		
+		this.metalLocations = []
+		for (let i = 0; i < this.width * this.width; i++) {
+			this.metalLocations.push(0)
+		}
+		
+		this.initialise()
+		this.randomise()
+		this.display()
+	}
+	
+	initialise() {
+		let numberPlaced = 0
+		for (let y = 0; y < this.width; y++) {
+			for (let x = 0; x < this.width; x++) {
+				if (numberPlaced < this.numberOfMetalPieces) {
+					this.setToMetal(x, y)
+					numberPlaced++
+				} else {
+					this.setToInsulator(x, y)
+				}				
+			}
+		}		
+	}
+	
+	randomise() {
+		for (let y = 0; y < this.width; y++) {
+			for (let x = 0; x < this.width; x++) {
+				let randomX = Math.floor(Math.random() * this.width)
+				let randomY = Math.floor(Math.random() * this.width)
+				this.swap(x, y, randomX, randomY)
+			}
+		}
+	}
+	
+	display() {
+		for (let y = 0; y < this.width; y++) {
+			for (let x = 0; x < this.width; x++) {
+				this.setImageData(x, y)
+			}
+		}
+		this.context.putImageData(this.imageData, 0, 0)
+		this.context.rect(3, 3, 6, 6)
+		this.context.stroke()
+	}
+	
+	setImageData(x, y) {
+		let data = this.imageData.data
+		let offset = this.offset(x, y)
+		let intensity = this.metalLocations[offset] * 255
+		console.log(intensity)
+		let dataOffset = offset * 4
+		data[dataOffset] = intensity
+		data[dataOffset + 1] = intensity
+		data[dataOffset + 2] = intensity
+		data[dataOffset + 3] = 255
+	}
+	
+	swap(x1, y1, x2, y2) {
+		let material1 = this.getMaterial(x1, y1)
+		let material2 = this.getMaterial(x2, y2)
+		this.setMaterial(x1, y1, material2)
+		this.setMaterial(x2, y2, material1)
+	}
+	
+	setToMetal(x, y) {
+		this.setMaterial(x, y, 1)
+	}
+	
+	setToInsulator(x, y) {
+		this.setMaterial(x, y, 0)
+	}
+	
+	setMaterial(x, y, material) {
+		let offset = this.offset(x, y)
+		this.metalLocations[offset] = material
+	}
+	
+	getMaterial(x, y) {
+		let offset = this.offset(x, y)
+		return this.metalLocations[offset]
+	}
+	
+	offset(x, y) {
+		return x + y * this.width
+	}
+}
+
+class TemperatureMap {
+	constructor(temperatureCanvas, radius) {
+		this.temperatureCanvas = temperatureCanvas
+		this.temperatureContext = this.temperatureCanvas.getContext('2d')
+	}
+	
+	display() {}
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	radiusInput = document.querySelector('#radius')
-	numberOfMetalPiecesInput = document.querySelector('#number_of_metal_pieces')
-	shapeCanvas = document.querySelector('#shape_canvas')
-	temperatureCanvas = document.querySelector('#temperature_canvas')
+	let radiusInput = document.querySelector('#radius')
+	let numberOfMetalPiecesInput = document.querySelector('#number_of_metal_pieces')
+	let heatsinkCanvas = document.querySelector('#heatsink_canvas')
+	let temperatureCanvas = document.querySelector('#temperature_canvas')
+	let simulation = null
+	let timerTracker = {timerId: 0}
 	
-	simulation = new Simulation(
-		radiusInput,
-		numberOfMetalPiecesInput,
-		shapeCanvas,
-		temperatureCanvas
-	)
-	
-	document.querySelector('#restart').addEventListener('click', () => {
-		simulation.restart()
-	})
+	let restart = () => {
+		clearTimeout(timerTracker.timerId)
+		let radius = radiusInput.value
+		let numberOfMetalPieces = numberOfMetalPiecesInput.value
+		let temperatureMap = new TemperatureMap(temperatureCanvas, radius)
+		let heatsink = new Heatsink(temperatureMap, heatsinkCanvas, radius, numberOfMetalPieces)
+		simulation = new Simulation(heatsink, timerTracker)
+	}
+	document.querySelector('#restart').addEventListener('click', restart)
 })
 
 
